b47a1e2cc6fcbd6c3849b4271ace021f397f6ee6
Fabian Suchett <Fabian@macbook-pro-de-fabian.local>
2016-05-14 15:39:21 -0500

    El cambio solo fue un comentario aunque no existe el archivo en github aun

---
+++ juegodeMemoria.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+//cambio remoto
+
+import UIKit
+
+var num = 15
+
+print(num % 5)
+
+for i in 0...100{
+    if i >= 30 && i <= 40{
+        print("viva swift")
+    }else if i % 5 == 0{
+        print("\(i) Bingo")
+    }else if i % 2 == 0{
+        print("\(i) Par")
+    }else{
+        print("\(i) Impar")
+    }
+}
